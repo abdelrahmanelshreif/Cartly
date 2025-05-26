c42facb984e686dbcac28972bf237ed564b5f732
Abdelrahman Mohamed Elshreif <106455463+abdelrahmanelshreif@users.noreply.github.com>
2025-05-26 14:05:27 -0700

    Configure Firebase at AppDelegete

--- Cartly/CartlyApp.swift
+++ Cartly/CartlyApp.swift
@@ -6,15 +6,51 @@
 //
 
 import SwiftUI
+import FirebaseCore
+
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
 
 @main
 struct CartlyApp: App {
     let persistenceController = PersistenceController.shared
 
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
-        }
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
+
+  var body: some Scene {
+    WindowGroup {
+      NavigationView {
+        ContentView()
+//              .environment(\.managedObjectContext, persistenceController.container.viewContext)
+      }
     }
+  }
 }
+
+//import SwiftUI
+//import Firebase
+//
+//@main
+//struct CartlyApp: App {
+//    let persistenceController = PersistenceController.shared
+//
+//    init(){
+//        FirebaseApp.configure()
+//    }
+//    var body: some Scene {
+//        WindowGroup {
+//            ContentView()
+//                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+//        }
+//    }
+//}
+//
+
