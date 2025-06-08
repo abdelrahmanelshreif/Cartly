692119b45c392b4b8cf98d36e4493aaf84f9f158
Abdelrahman Mohamed Elshreif <106455463+abdelrahmanelshreif@users.noreply.github.com>
2025-06-08 08:27:02 -0700

    fix flikering issue at navigation

--- Cartly/Presentation/Screens/HomeScreen/HomeTabView.swift
+++ Cartly/Presentation/Screens/HomeScreen/HomeTabView.swift
@@ -1,28 +1,29 @@
 import SwiftUI
 
 struct HomeTabView: View {
+    @State private var selectedTab = 0
     var body: some View {
-        TabView {
+        TabView(selection: $selectedTab){
             HomeScreen()
                 .tabItem {
                     Image(systemName: "house")
                     Text("Home")
-                }
+                }.tag(1)
             CategoryScreen()
                 .tabItem {
                     Image(systemName: "bag")
                     Text("Categories")
-                }
+                }.tag(2)
             WishlistScreen()
                 .tabItem {
                     Image(systemName: "heart.fill")
                     Text("Favourites ")
-                }
+                }.tag(3)
             ProfileScreen()
                 .tabItem {
                     Image(systemName: "person.crop.circle.fill")
                     Text("Profile ")
-                }
+                }.tag(4)
         }
         .accentColor(.black)
     }
